ba6a20c631826c1a2a0a30dea203691ba2455dc0
haliron <abdulhalimdemir86@gmail.com>
2023-07-04 15:42:23 +0300

    C-Sharp-MVC-Web-Site
    I made a web site with using c# and MVC.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Services()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        public ViewResult BlogOne()
+        {
+            return View();
+        }
+
+        public ViewResult BlogTwo()
+        {
+            return View();
+        }
+
+        public ViewResult CloumnOne()
+        {
+            return View();
+        }
+
+        public ViewResult CloumnTwo()
+        {
+            return View();
+        }
+
+
+        public ViewResult CloumnThree()
+        {
+            return View();
+        }
+
+        public ViewResult CloumnFour()
+        {
+            return View();
+        }
+    }
+}
